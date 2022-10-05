7745f87192242633ebc8c2d74428d7278eec31b1
Jackie Walker <JK@Livias-MacBook-Pro.local>
2022-10-05 11:07:16 -0500

    Added first consolelog

---
+++ js/script.js
@@ -0,0 +1,40 @@
+/* Syntax Basics 
+White Space
+
+
+/*Comments*/
+//Single Line Comment
+
+/* Multi-
+line 
+comment
+*/
+
+/*JS Multi-line Comment*/
+//JS Single line Comment
+
+/*Semicolons */
+document.title = "Hello World!";
+document.title = "I'm going to win";
+/*****/
+
+/*Case Sensitivity*/
+//*Pascal Case
+//FirstName = "Jackie";
+//firstname = "Jackie";
+//Camel Case
+firstName = "Jackie";
+//first_name ="Jackie";
+/*****/
+
+console.log(firstName);
+console.log(29);
+
+
+/* STRINGS */
+
+console.log("I'm'")
+console.log("I\'m'")
+console.log("Hello World!" + "This is awesome!");
+console.log("Jackie".length); //6
+console.log("Jackie 8        ".length) //16
